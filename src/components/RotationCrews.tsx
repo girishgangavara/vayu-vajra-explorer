026ad7aff36523c1ad0e8f520d5241bdefa295dc
Girish E <girish@techfino.in>
2026-01-11 11:58:07 +0530

    Photos Push

--- src/components/RotationCrews.tsx
+++ src/components/RotationCrews.tsx
@@ -21,7 +21,7 @@ const crewData = [
     busNumber: "KIA-15/3",
     crews: [
       { name: "(451) Raghunathreddy", photo: "https://lfuthmexacgvbyjufbih.supabase.co/storage/v1/object/public/vayuvajra/Raghunathreddy.jpeg", days: ["Monday", "Wednesday", "Friday"] },
-      { name: "(403) Chandrashekar", photo: "https://firebasestorage.googleapis.com/v0/b/kia-5d-crewlist.appspot.com/o/IMG_20240425_221946_907-removebg-preview.png?alt=media&token=c90da2ea-88f0-4da6-b906-151454d3617d", days: ["Tuesday", "Thursday", "Saturday"] },
+      { name: "(403) Chandrashekar Reddy", photo: "https://firebasestorage.googleapis.com/v0/b/kia-5d-crewlist.appspot.com/o/IMG_20240425_221946_907-removebg-preview.png?alt=media&token=c90da2ea-88f0-4da6-b906-151454d3617d", days: ["Tuesday", "Thursday", "Saturday"] },
       { name: "(5483) Keshava", photo: "https://lfuthmexacgvbyjufbih.supabase.co/storage/v1/object/public/vayuvajra/keshava.png", days: ["Sunday"] },
     ]
   },
